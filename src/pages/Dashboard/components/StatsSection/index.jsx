1e5625e7a39a0fd64f7331e2588d1563848109f6
Abdallah-Salama2 <afas200030@gmail.com>
2026-05-06 06:51:09 +0300

    Readme

--- src/pages/Dashboard/components/StatsSection/index.jsx
+++ src/pages/Dashboard/components/StatsSection/index.jsx
@@ -1,34 +1,23 @@
 import { useContext } from "react";
 import { DashboardContext } from "../../../../context/DashboardContext";
-
+import HighlitedCard from "../Header/components/HighlitedCard";
+import StatsCard from "../../../../components/StatsCard";
 export default function StatsSection() {
   const { stats, loading } = useContext(DashboardContext);
 
   if (loading) return <p>Loading stats...</p>;
 
   return (
-    <div className="row g-3 mb-4">
-      {stats.map((item) => (
-        <div className="col-md-4" key={item.id}>
-          <div
-            className="card p-3 border-0 shadow-sm"
-            style={{ borderRadius: "15px" }}
-          >
-            <p className="text-secondary small mb-1">{item.title}</p>
-            <h3 className="fw-bold mb-1">
-              {item.value}
-              <span className="fs-6 text-secondary fw-normal">
-                {item.decimal}
-              </span>
-            </h3>
-            <span
-              className={
-                item.isPositive ? "text-success small" : "text-danger small"
-              }
-            >
-              {item.isPositive ? "↑" : "↓"} {item.percentage}
-            </span>
-          </div>
+    <div className="row g-4 mt-2">
+      {/* الكارت الملون */}
+      <div className="col-12 col-md-6 col-xl-3">
+        <HighlitedCard />
+      </div>
+
+      {/* كروت الإحصائيات (Mapping) */}
+      {stats.map((item, index) => (
+        <div key={index} className="col-12 col-md-6 col-xl-3">
+          <StatsCard {...item} />
         </div>
       ))}
     </div>
